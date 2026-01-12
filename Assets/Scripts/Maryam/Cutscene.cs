7269e690f931d5776e473c9490e4bbf72f741e7a
MaryamMMKB <154224825+MaryamMMKB@users.noreply.github.com>
2026-01-12 12:00:29 +0100

    Animation for found objects
    short animation for finding objects. have not YET animated th 3 other cutscenes

--- Assets/Scripts/Maryam/Cutscene.cs
+++ Assets/Scripts/Maryam/Cutscene.cs
@@ -2,15 +2,42 @@ using UnityEngine;
 
 public class Cutscene : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    public Animator Cutscenes;
+    public PlayerMovement PlayerMovement;
     void Start()
     {
         
     }
-
-    // Update is called once per frame
-    void Update()
+    void Awake()
     {
-        
+        if (Cutscenes == null)
+        {
+            Cutscenes = GetComponent<Animator>();
+        }
+    }
+
+    public void PlayJVV() 
+    {
+     PlayerMovement.enabled = false;
+        Cutscenes.SetTrigger("JVV");
+    }
+    public void PlayBTC() 
+    {
+        PlayerMovement.enabled = false;
+        Cutscenes.SetTrigger("BTC");
+    }
+    public void PlayAE() 
+    {
+        PlayerMovement.enabled = false;
+        Cutscenes.SetTrigger("AE");
+    }
+    public void PlayOF() 
+    {
+        PlayerMovement.enabled = false;
+        Cutscenes.SetTrigger("OF");
+    }
+    public void EndCutscene()
+    {
+        PlayerMovement.enabled = true;
     }
 }
